2e6545109ab01c690ed929dd9c5e8d4754fc32d9
Fran <fran.p.21@gmail.com>
2021-01-30 18:08:29 -0300

    Cambia el test para generar el tableron con una función en vez de pasar una variable estática

--- utils/placeBombs.test.js
+++ utils/placeBombs.test.js
@@ -1,73 +1,18 @@
+import createNewBoard from './createNewBoard'
 import placeBombs from './placeBombs'
 
 it('Verifica que se inserten las minas en el tablero', () => {
-  const board = [
-    [
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-    ],
-    [
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-    ],
-    [
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-      {
-        isBomb: false,
-        clicked: false,
-        id: '',
-        flagged: false,
-      },
-    ],
-  ]
   const boardHeight = 3
   const boardWidth = 3
   const expectedMines = 4
-  const newBoard = placeBombs(board, expectedMines, boardWidth, boardHeight)
-  const minesArray = newBoard.map(row => {
+  const newBoard = createNewBoard(boardHeight, boardWidth)
+  const boardWithMines = placeBombs(
+    newBoard,
+    expectedMines,
+    boardHeight,
+    boardWidth,
+  )
+  const minesArray = boardWithMines.map(row => {
     return row.filter(item => {
       return item.isBomb === true
     }).length
